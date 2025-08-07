0ff441928b54fa2e5a79738d76cd582adedfaeeb
Dusan Ilic <dusan.ilic1999@gmail.com>
2025-08-07 13:41:28 +0200

    fix: fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouterhRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
